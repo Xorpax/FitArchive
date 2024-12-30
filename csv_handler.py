366f7dc6969d62aa81cdba1b031f1938f2bfe91d
Xorpax <petarda795@gmail.com>
2024-12-30 22:31:25 +0000

    Refactor workout to exercise

--- csv_handler.py
+++ csv_handler.py
@@ -5,7 +5,7 @@ import datetime
 class Handler:
     def __init__(self, csv_path: str) -> None:
         self.csv_path = csv_path
-        self.workouts = []
+        self.exercises = []
     
     def get_dataset(self) -> pd.DataFrame:
         try:
@@ -13,21 +13,21 @@ class Handler:
         except Exception as e:
             return e
     
-    def update_workouts(self) -> None:
-        current_workouts = self.get_dataset().columns
-        self.workouts = current_workouts.values
+    def update_exercises(self) -> None:
+        current_exercises = self.get_dataset().columns
+        self.exercises = current_exercises.values
 
-    def add_workout(self, workout_name: str) -> str:
-        self.update_workouts()
-        workout_name = workout_name.capitalize()
+    def add_exercise(self, exercise_name: str) -> str:
+        self.update_exercises()
+        exercise_name = exercise_name.capitalize()
         dataset = self.get_dataset()
-        if workout_name in self.workouts:
-            return f"Entry for {workout_name} already exists!"
-        dataset[workout_name] = ""
+        if exercise_name in self.exercises:
+            return f"Entry for {exercise_name} already exists!"
+        dataset[exercise_name] = ""
         dataset.to_csv(self.csv_path, index=False)
-        return f"Entry for {workout_name} has been successfully created."
+        return f"Entry for {exercise_name} has been successfully created."
     
-    def add_workout_data(self, workout_name: str, score: float, date: str):
+    def add_exercise_data(self, exercise_name: str, score: float, date: str):
         raise NotImplementedError("This method will add scores (weight or reps) along with dates to the appropriate exercise")
 
 # open csv file and read it, displaying contents in a gui
