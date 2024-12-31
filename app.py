f4758faa62eab4a0d4a619ab6bafc9fc5337bfb8
Xorpax <petarda795@gmail.com>
2024-12-31 19:47:42 +0000

    Add further unit testing

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
 from csv_handler import Handler
+from gui import App
 
 if __name__ == "__main__":
     file_path = r".\example1.csv"
-    wrong_path = r"..\example1.csv"
     h = Handler(csv_path=file_path)
-    print(h.get_dataset())
+    # h.add_exercise("Pull ups")
+    # h.add_exercise_data("Pull ups", score=15, date="12.12.2024", units="reps")
+    # h.add_exercise_data("Pull ups", score=16, date="13.12.2024", units="reps")
+    # h.add_exercise_data("Pull ups", score=18, date="14.12.2024", units="reps")
+    # print(h.get_dataset())
+    # h.remove_exercise("pull ups")
+    # h.add_exercise_data("Goblet squats", score=7, date="14.12.2024", units="kg")
+    # h.add_exercise_data("Goblet squats", score=7, date="16.12.2024", units="kg")
+    # print(h.get_dataset())
+    # h.remove_record(exercise_name="goblet squats", row_index=1)
+    # print(h.get_dataset())
+    app = App()
+    app.mainloop()
