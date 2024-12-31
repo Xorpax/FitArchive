3fe9a09625cc59d136906e86600a81dfaa7fe688
Xorpax <petarda795@gmail.com>
2024-12-31 10:25:46 +0000

    Finish add_exercise_data

--- csv_handler.py
+++ csv_handler.py
@@ -7,6 +7,7 @@ class Handler:
     def __init__(self, csv_path: str) -> None:
         self.csv_path = csv_path
         self.exercises = []
+        self.update_exercises()
     
     def get_dataset(self) -> pd.DataFrame:
         try:
@@ -28,29 +29,24 @@ class Handler:
         self.update_exercises()
         return f"Entry for {exercise_name} has been successfully created."
     
-    def add_exercise_data(self, exercise_name: str, score: float, date: str, units:str):
+    def add_exercise_data(self, exercise_name: str, score: float, date: str, units:str) -> str:
         exercise_name = exercise_name.capitalize()
         dataset = self.get_dataset()
-        if exercise_name not in dataset:
+        correct_datatypes = (isinstance(score, float) or isinstance(score, int)) and isinstance(date, str) and isinstance(units, str)
+        if exercise_name not in self.exercises:
             return f"No entry for {exercise_name}. Aborting"
         try:
-            assert (isinstance(score, float) or isinstance(score, int)) and isinstance(date, str) and isinstance(units, str)
+            assert correct_datatypes
         except AssertionError:
             return f"Invalid data types"
         
-        exercise_dict = dataset[exercise_name].to_dict()
-        print(f"{exercise_dict=}")
-        # print(dataset[exercise_name].dropna())
-        number_of_entries = len(dataset[exercise_name].dropna())
-        print(f"{number_of_entries=}")
+        exercise_col = dataset[exercise_name].dropna()
+        entry_index = len(exercise_col)
         entry = f"{score}{units}|{date}"
-        print(f"{entry=}")
-        dataset.loc[number_of_entries, exercise_name] = entry
-        print("---------")
+        dataset.loc[entry_index, exercise_name] = entry
         dataset.to_csv(self.csv_path, index=False)
-        # print(dataset)
         self.update_exercises()
-        # raise NotImplementedError("This method will add scores (weight or reps) along with dates to the appropriate exercise")
+        return f"Entry: {entry} successfully added for {exercise_name}"
 
 # open csv file and read it, displaying contents in a gui
 # allow adding scores with dates (writing to the csv)
