01ad253dd552420e15a1f2cf2cacbcd43d1abeb4
Xorpax <petarda795@gmail.com>
2025-01-10 21:31:57 +0000

    Clean up app.py

--- app.py
+++ app.py
@@ -1,12 +1,7 @@
-from csv_handler import Handler
 from gui import App
 
 if __name__ == "__main__":
-    try:
-        file_path = r".\example1.csv"
-        empty_csv_path = r".\exercises.csv"
-        h = Handler(csv_path=file_path)
-        FitArchive = App(csv_path=file_path)
-        FitArchive.mainloop()
-    except KeyboardInterrupt:
-        print("Closing...")
+    empty_csv_path = r".\exercises.csv"
+    mine = r".\private.csv"
+    FitArchive = App(csv_path=mine)
+    FitArchive.mainloop()
