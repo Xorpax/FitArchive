c7366fd04ed62f1484b43eaa21986bf2c420da2d
Xorpax <petarda795@gmail.com>
2025-01-16 16:45:39 +0000

    Clean up add_exercise

--- csv_handler.py
+++ csv_handler.py
@@ -21,18 +21,15 @@ class Handler:
             current_exercises = self.get_dataset().columns
             self.exercises = current_exercises.values
 
-    def add_exercise(self, exercise_name: str, category: str, units: str) -> str:
+    def add_exercise(self, exercise_name: str, category: str, units: str) -> None:
         exercise_name = exercise_name.capitalize()
         dataset = self.get_dataset()
-        if exercise_name in self.exercises:
-            return f"Entry for {exercise_name} already exists!"
         if self.is_csv_empty():
             dataset = pd.DataFrame({exercise_name: [f"PR:0|{units}", f"Category:{category}", "Note:"]})
         else:
             dataset[exercise_name] = pd.Series([f"PR:0|{units}", f"Category:{category}", "Note:"])
         dataset.to_csv(self.csv_path, index=False)
         self.update_exercises()
-        return f"Entry for {exercise_name} has been successfully created."
     
     def add_exercise_data(self, exercise_name: str, score: float, date: str, units:str) -> str:
         exercise_name = exercise_name.capitalize()
