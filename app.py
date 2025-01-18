caba832e8c90686c729bacccf0839584a12681bd
Xorpax <petarda795@gmail.com>
2025-01-18 21:33:35 +0000

    Update app.py

--- app.py
+++ app.py
@@ -5,5 +5,5 @@ if __name__ == "__main__":
     current_dir = os.getcwd()
     empty_csv_path = os.path.join(current_dir, "exercises.csv")
     mine = os.path.join(current_dir, "private.csv")
-    FitArchive = App(csv_path=mine)
+    FitArchive = App(csv_path=empty_csv_path)
     FitArchive.mainloop()
